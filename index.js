dafbfd43dd66765c2d596c0db2ece9d000b78e9a
emarubi <69473685+emarubi@users.noreply.github.com>
2022-05-07 17:13:36 +0200

    adding files form moovisfinder repository

---
+++ index.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv')
+dotenv.config();
+const cors = require('cors')
+const Redis = require('redis')
+
+// const runApp = async () =>  {
+
+    const PORT = process.env.PORT || 4000;
+
+    const express = require('express')
+    const app = express()
+    const path = require('path')
+
+    app.use(cors("*"))
+
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    const router = require('./app/router')
+    app.use(router)
+
+    app.set('views', path.join(__dirname, 'app/views'))
+    app.set('view engine', 'ejs')
+
+
+    app.listen(PORT, () => {
+        console.log(`server started at port ${PORT}`)
+    })
+// }
+
+
+// runApp()
